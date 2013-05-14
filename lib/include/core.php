beba0f54003b03a5e4b14d465a0b480921c7f6f6
flrngel <flrngel@gmail.com>
2013-05-14 11:27:22 +0900

    json type

--- lib/include/core.php
+++ lib/include/core.php
@@ -1,6 +1,11 @@
 <?
 	function render($filename){
 		global $res;
+		if( $_REQUEST['dataType'] == "json" )
+		{
+			echo json_encode($resa);
+			return;
+		}
 		$path=$_SERVER['DOCUMENT_ROOT']."/app/views/$filename";
 		include $path;
 	}
