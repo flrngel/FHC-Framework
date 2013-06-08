d1b71fdf7cf4cc02d9e4ddfa9a4aed20ae1b59cf
flrngel <flrngel@gmail.com>
2013-06-08 17:47:32 +0900

    render layout

--- lib/include/core.php
+++ lib/include/core.php
@@ -1,5 +1,5 @@
 <?
-	function render($filename){
+	function render($filename,$layout="default.html"){
 		global $res;
 		if( $_REQUEST['dataType'] == "json" )
 		{
@@ -7,6 +7,13 @@
 			return;
 		}
 		$path=$_SERVER['DOCUMENT_ROOT']."/app/views/$filename";
+		$path_layout=$_SERVER['DOCUMENT_ROOT']."/app/views/layouts/".$layout;
+
+		ob_start();
 		include $path;
+		$contents=ob_get_contents();
+		ob_end_clean();
+
+		include $path_layout;
 	}
 ?>
