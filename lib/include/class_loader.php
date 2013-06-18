ff3e0ef1b1e9036137516767137020b9eeac8551
flrngel <flrngel@gmail.com>
2013-06-18 09:15:46 +0900

    inc to php
    	renamed:    lib/include/class_loader.inc -> lib/include/class_loader.php

--- lib/include/class_loader.php
+++ lib/include/class_loader.php
@@ -1,6 +1,6 @@
 <?
 	function __autoload($name){
-		$file = $_SERVER['DOCUMENT_ROOT']."/lib/classes/class.$name.inc";
+		$file = $_SERVER['DOCUMENT_ROOT']."/lib/classes/class.$name.php";
 		if( !file_exists($file) ){
 			echo "미정의 클래스명 : $name , ".$_SERVER['PHP_SELF'];
 			return 0;
