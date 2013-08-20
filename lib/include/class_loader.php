488158eb5644806db696d33da4d746becc0c6de9
flrngel <flrngel@gmail.com>
2013-08-20 16:59:55 +0900

    class_loader for big sites

--- lib/include/class_loader.php
+++ lib/include/class_loader.php
@@ -1,5 +1,6 @@
 <?
 spl_autoload_register(function($class){
+	$class=str_replace("\\","/",$class);
 	$file = $_SERVER['DOCUMENT_ROOT']."/lib/classes/$class.php";
 	if( !file_exists($file) ){
 		echo "there's no class : $class , ".$_SERVER['PHP_SELF'];
