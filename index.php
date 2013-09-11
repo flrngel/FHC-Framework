aa50cd172be48c43cba6fa7b607715f30865b9e1
flrngel <flrngel@gmail.com>
2013-09-11 10:56:20 +0900

    index indent fixed

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?
-	$_SERVER['DOCUMENT_ROOT'] = dirname(__FILE__);
-	
-	ini_set('display_errors', 1); 
-	error_reporting(E_ALL^E_NOTICE^E_WARNING);
+$_SERVER['DOCUMENT_ROOT'] = dirname(__FILE__);
+
+ini_set('display_errors', 1); 
+error_reporting(E_ALL^E_NOTICE^E_WARNING);
 
 
-	$page=$_GET['fhc_page'];
-	if( empty($page) ) $page='index';
+$page=$_GET['fhc_page'];
+if( empty($page) ) $page='index';
 
-	$path=$_SERVER['DOCUMENT_ROOT']."/app/controllers/";
-	$viewpath_default=$page.".html";
+$path=$_SERVER['DOCUMENT_ROOT']."/app/controllers/";
+$viewpath_default=$page.".html";
 
-	$job=$path.$page.".php";
-	if( !is_file($job) ){
-		if( is_dir($path.$page) ){
-			$job=$path.$page."/index.php";
-			$viewpath_default=$page."/index.html";
-		}
+$job=$path.$page.".php";
+if( !is_file($job) ){
+	if( is_dir($path.$page) ){
+		$job=$path.$page."/index.php";
+		$viewpath_default=$page."/index.html";
 	}
+}
 
-	if( !preg_match("/^".preg_quote($path,'/')."/" , realpath($job)) ) exit;
+if( !preg_match("/^".preg_quote($path,'/')."/" , realpath($job)) ) exit;
 
-	$res=Array();
+$res=Array();
 
-	foreach(glob("lib/include/*.*") as $filename) include($filename);
+foreach(glob("lib/include/*.*") as $filename) include($filename);
 
-	include $job;
+include $job;
 
-	if( !defined("__RENDERED__") ){
-		render($viewpath_default);
-	}
+if( !defined("__RENDERED__") ){
+	render($viewpath_default);
+}
 ?>
